54fb1b32e01692bdaf6d6c3805d78500aaac1564
Fraasi <doofassi@gmail.com>
2021-04-08 12:03:27 +0300

    refactor & cleanup

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,11 +1,9 @@
 import "../css/popup.css";
 import getLinks from './popup/getlinks.js'
-import copyImg from '../img/clipboard.svg'
-console.log('copyImg:', copyImg)
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  /* Add your event listeners here */
+
   chrome.tabs.executeScript({
     code: `(${getLinks.toString()})()`
   })
@@ -27,32 +25,32 @@ chrome.runtime.onMessage.addListener(
       "from a content script:" + sender.tab.url :
       "from the extension")
 
-      Object.keys(message)
-        .forEach(key => {
-          if (message[key].length) {
-            const el = document.getElementById(key)
-            el.addEventListener('click', e => {
-              if (e.target.tagName === 'BUTTON') {
-                const link = e.target.getAttribute('data-link')
-                navigator.clipboard.writeText(link)
-                const link_span = document.querySelector((`span[data-link="${link}"]`))
-                link_span.innerHTML = 'Copied to clipboard<br>'
-                setTimeout(function() {
-                  link_span.innerHTML = `${link}<br>`;
-                }, 750);
-              }
+    Object.keys(message)
+      .forEach(key => {
+        if (message[key].length) {
+          const el = document.getElementById(key)
+
+          message[key].forEach(link => {
+            const button = document.createElement('button')
+            button.setAttribute('type', 'button')
+            button.setAttribute('class', 'copy-to-clipboard')
+            button.setAttribute('data-link', link)
+            el.append(button)
+            const span = document.createElement('span')
+            span.setAttribute('data-link', link)
+            span.innerHTML = `${link}<br>`
+            el.append(span)
+
+            button.addEventListener('click', e => {
+              const link = e.target.getAttribute('data-link')
+              navigator.clipboard.writeText(link)
+              const link_span = document.querySelector((`span[data-link="${link}"]`))
+              link_span.innerHTML = 'Copied to clipboard<br>'
+              setTimeout(function () {
+                link_span.innerHTML = `${link}<br>`;
+              }, 750);
             })
-            message[key].forEach(link => {
-              const button = document.createElement('button')
-              button.setAttribute('type', 'button')
-              button.setAttribute('class', 'copy-to-clipboard')
-              button.setAttribute('data-link', link)
-              el.append(button)
-              const span = document.createElement('span')
-              span.setAttribute('data-link', link)
-              span.innerHTML = `${link}<br>`
-              el.append(span)
-            })
-          }
-        })
+          })
+        }
+      })
   })
--- src/js/popup/getlinks.js
+++ src/js/popup/getlinks.js
@@ -3,101 +3,45 @@ function getLinks() {
   const iFrames = []
   const iFramesInner = []
   const videoTags = []
-  const noYTDL = []
   const m3u8s = []
 
-  const filterOut = [ // does not work with YT-DL
-    'abcvideo',
-    'cloudvideo',
-    'dood',
-    'fmoviesfree',
-    'gamovideo',
-    'jetload',
-    'mycloud',
-    'mystream',
-    'powvideo',
-    'streamcherry',
-    'streamplay',
-    'streamtape',
-    'supervideo',
-    'thevideo',
-    'upstream',
-    'vev',
-    'videobin',
-    'vidbull',
-    'vidcloud',
-    'vidia',
-    'vidlox',
-    'vidoza',
-    'vidup',
-    'vidzi',
-    'vshare',
-  ]
 
-  const toRemove = [
+  const filterOut = [
     'disqus',
     'twitter',
-    'google'
+    'google',
+    'facebook'
   ]
 
   function remove(str) {
-    return toRemove.some(el => {
+    if (str.length === 0) return true
+    return filterOut.some(el => {
       const regex = new RegExp(el)
       return regex.test(str)
     })
-
   }
+
 
   Array.from(document.getElementsByTagName('iframe'))
     .forEach((iframe) => {
-      let src = remove(iframe.src) ? '' : iframe.src
-      let filtered = filterOut.find(filter => src.includes(filter))
-      filtered ? noYTDL.push(src) : iFrames.push(src)
+      if (!remove(iframe.src)) iFrames.push(iframe.src)
       if (iframe.contentDocument && iframe.contentDocument.querySelector('iframe')) {
-        let innerSrc = iframe.contentDocument.querySelector('iframe').src
-        innerSrc = remove(innerSrc) ? '' : innerSrc
-        let filtered = filterOut.find(filter => innerSrc.includes(filter))
-        filtered ? noYTDL.push(innerSrc) : iFramesInner.push(innerSrc)
+        const innerSrc = iframe.contentDocument.querySelector('iframe').src
+        if (!remove(innerSrc)) iFramesInner.push(innerSrc)
       }
     })
 
-  // console.log('Iframe source(s):')
-  // iFrames.forEach(src => { console.log(src) })
-  // console.log('Iframe inner source(s):')
-  // iFramesInner.forEach(src => { console.log(src) })
-
   //rewrite when you stumble upon a video tag again
-  // console.log('Video source(s)')
   Array.from(document.getElementsByTagName('video'))
     .forEach((el) => {
       let source = 'No el.src || &lt;source&gt;'
       if (!el.src && el.firstElementChild) {
         source = el.firstElementChild.src
-        // console.log('el.first...')
       } else if (el.src) {
         source = el.src
-        // console.log('el.src')
       }
-
-      let filtered = filterOut.find(filter => source.includes(filter))
-      filtered ? noYTDL.push(source) : videoTags.push(source)
-
-      // console.log(source)
+      if (source !== null) videoTags.push(source)
     })
-
-
-  //   attempt to fetch inner iFrames..
-  // if (!iFramesInner.length && iFrames[0]) {
-  //   fetch(iFrames[0], {
-  //     'mode': 'no-cors',
-  //     'headers': new Headers({
-  //       'Content-Type': 'text/plain'
-  //     })
-  //   })
-  //     .then(rawhtml => rawhtml.text())
-  //     .then(html => { console.log('done', html) })
-  //     .catch(err => { throw err })
-  // }
 
 
   // captureNetworkRequests, ie. m3u8's
@@ -117,7 +61,6 @@ function getLinks() {
     iFramesInner,
     videoTags,
     m3u8s,
-    noYTDL
   }, function (response) {
     console.log(response)
   })
